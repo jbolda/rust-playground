e39d287b2b19679ae36d203460de87548f28d427
Jacob Bolda <me@jacobbolda.com>
2020-07-17 21:54:27 -0500

    variables

---
+++ variables/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+    another_function();
+}
+
+fn another_function() {
+    println!("Another function.");
+
+    let x = plus_one(5);
+
+    println!("The value of x is: {}", x);
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
+
